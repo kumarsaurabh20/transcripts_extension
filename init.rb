9f3665c7afb5a43f1ea789d886a6e739646a0707
Kumar Saurabh Singh <kumarsaurabh20@gmail.com>
2016-10-23 17:50:49 +0100

    init

--- init.rb
+++ init.rb
@@ -1,21 +1,9 @@
 #!/usr/bin/ruby
-
 ##Transcript Extension###
-
 ##Launch this file from the command line to start the application and get started###
 
 APP_ROOT = File.dirname(__FILE__)
 $:.unshift(File.join(APP_ROOT, 'lib'))
-
 require 'extension'
-#require "Testup"
-
-
 map = Extension.new
-#map.options(ARGV)
 map
-#test = Testdir.new
-#test.runCommand("cat", "file1", "file2", "file3", "file4")
-
-#test = Testup.new
-#test.check
